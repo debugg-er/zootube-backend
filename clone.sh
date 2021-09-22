12a934679cbfa61251ab5fc3da0ebca18d51c8d0
backy4rd <khainguyen9209@gmail.com>
2021-09-22 20:17:34 +0700

    update

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
-rm -rf api static
+rm -rf api media
 
 git clone https://github.com/backy4rd/zootube-api api
-git clone https://github.com/backy4rd/zootube-static-server static
+git clone https://github.com/backy4rd/zootube-static-server media
