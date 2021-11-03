1e71979e6b6c06f8bf13167793b0382f5545bb87
backy4rd <khainguyen9209@gmail.com>
2021-11-03 09:59:10 +0700

    add rtmp service

--- clone.sh
+++ clone.sh
@@ -1,5 +1,6 @@
-rm -rf api media chat
+rm -rf api media chat live
 
 git clone https://github.com/backy4rd/zootube-api api
 git clone https://github.com/backy4rd/zootube-static-server media
 git clone https://github.com/backy4rd/zootube-chat chat
+git clone https://github.com/backy4rd/zootube-live live
